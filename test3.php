b90ca62227444420ef1522793336d1cc1237d027
Adi Iacob <iacobadrian@yahoo.com>
2016-12-26 20:46:20 +0200

    "modif 5"

--- test3.php
+++ test3.php
@@ -7,3 +7,20 @@
  <?php echo '<p>Hello World1</p>'; ?>  
  </body>
 </html>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World1</p>'; ?>  
+ </body>
+</html><html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World1</p>'; ?>  
+ </body>
+</html>
