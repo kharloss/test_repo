aa9132d118baf9ef49f51607dbb432a27902562f
Adi Iacob <iacobadrian@yahoo.com>
2016-12-26 20:38:00 +0200

    Commit #3

--- test2.php
+++ test2.php
@@ -4,5 +4,8 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World1</p>'; ?> 
+ <?php echo '<p>Hello World2</p>'; ?> 
+ <?php echo '<p>Hello World3</p>'; ?> 
  </body>
 </html>
