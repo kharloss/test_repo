c4a402777741da78dc9fc3131de20ffbb81d9b7d
Adi Iacob <iacobadrian@yahoo.com>
2016-12-26 20:58:34 +0200

    Revert ""modif 5""
    This reverts commit b90ca62227444420ef1522793336d1cc1237d027.

--- test3.php
+++ test3.php
@@ -7,20 +7,3 @@
  <?php echo '<p>Hello World1</p>'; ?>  
  </body>
 </html>
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- <?php echo '<p>Hello World1</p>'; ?>  
- </body>
-</html><html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- <?php echo '<p>Hello World1</p>'; ?>  
- </body>
-</html>
